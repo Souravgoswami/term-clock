1dd7e1dee35df141a8d8d1957bce375977be8c6c
Sourav Goswami <souravgoswami@protonmail.com>
2019-09-24 16:54:39 +0530

    Fixed Bugs and Reduced Memory Consumption
    1. Fixed Bugs
    2. Reduced the memory consumed by this application

--- term-clock.rb
+++ term-clock.rb
@@ -9,7 +9,7 @@ abort("#{File.basename($0)} didn't find any terminal. You can run `#{File.basena
 abort("You are using #{RUBY_ENGINE.capitalize} #{RUBY_VERSION}, which is incompatible. Atleast Ruby 2.5 is Recommended...") if RUBY_VERSION.split(?.).first(2).join.to_i < 25
 
 # Version and files
-VERSION = '0.50'
+VERSION = '0.51'
 
 CHARACTERS = File.join(__dir__, %w(term-clock characters.txt))
 QUOTE = File.join(__dir__, %w(term-clock quotes.txt))
@@ -21,10 +21,15 @@ CONFIGURATION = File.join(__dir__, %w(term-clock term-clock.conf))
 
 # Stop flickering the clock while running in the TTYs!
 CLEAR = ENV.key?('COLORTERM') ? "\e[3J\e[H\e[2J" : "\e[H"
+$-n, $-s = ?\n, ?\s
+CPU_USAGE = '/proc/stat'
+MEMINFO = '/proc/meminfo'
+SWAPINFO = '/proc/swaps'
+
 print "\e[2J\e[H\e[J" unless ENV.key?('COLORTERM')
 
 # Define method then if not defined
-Kernel.class_exec { define_method(:then) { |&b| b.call(self) } } unless defined?(Kernel.then)
+Kernel.class_exec { define_method(:then) { |&b| b === self } } unless defined?(Kernel.then)
 GC.start(full_mark: true, immediate_sweep: true)
 require('io/console')
 
@@ -34,10 +39,10 @@ String.define_method(:colourize) do |colours: [208, 203, 198, 164, 129, 92], ani
 	each_line do |str|
 		colour, len = c, str.length
 		colour_size = colour.size - 1
-		div, i, index = str.length./(colour_size.next).then { |x| x.zero? ? 1 : x }, -1, 0
+		div, i, index = str.length./(colour_size.next).then { |x| x == 0 ? 1 : x }, -1, 0
 
 		while i < len
-			index += 1 if (i += 1) > 1 && i.%(div).zero? && index < colour_size
+			index += 1 if (i += 1) > 1 && i.%(div).==(0) && index < colour_size
 			final << "\e[38;5;#{colour[index]}m#{str[i]}"
 
 			if animate
@@ -62,7 +67,7 @@ end unless ARGV.any? { |x| x[/^\-\-no\-colour$/] }
 
 String.define_method(:colourize) { |arg = nil| self.replace("\e[0m#{self}") } if ARGV.any? { |x| x[/^\-\-no\-colour$/] }
 
-Float.define_method(:rpad) { |round = 2| round(round).to_s.then { |x| x.split(?.)[1].length.then { |y| y < round ? x << ?0.*(round - y) : x } } }
+Float.define_method(:rpad) { |round = 2| round(round).to_s.then { |x| x.split(?.)[1]&.length&.then { |y| y < round ? x << ?0.*(round - y) : x } }.to_s }
 
 def generate_files(file, url, permission = 0644)
 	begin
@@ -88,13 +93,13 @@ def generate_files(file, url, permission = 0644)
 	rescue Errno::EACCES
 		abort "Cannot write to #{file}. Permission denied.\nPlease try running #{$0} as root".colourize
 	rescue SignalException, Interrupt, SystemExit
-		abort "Downloading is aborted. This may also lead to corrupted data.".colourize
+		abort "\nDownloading is aborted. This may also lead to corrupted data.".colourize
 	rescue SocketError, OpenSSL::SSL::SSLError
 		abort "Can't download #{file}. Is there any connection issue?".colourize
 	rescue Exception => e
 		puts(e.full_message)
 	ensure
-		t.kill if t
+		t&.kill
 	end
 
 	STDERR.puts "Generated #{file} file successfully.".colourize
@@ -111,46 +116,48 @@ def main
 
 	characters = IO.read(CHARACTERS).split(/#+/).reject(&:empty?).reduce({}) do |x, y|
 		lines = y.strip.split(y.lstrip[0])[1]
-		max = lines.each_line.map(&:length).max.next
-		x.merge!(y.lstrip[0] => lines.each_line.map { |z| z.chomp.ljust(max) + ?\n }.join)
+		max = lines.lines.map!(&:length).max + 1
+		x.merge!(y.lstrip[0] => lines.lines.map! { |z| z.chomp.ljust(max) + $-n }.join)
 	end.freeze
 
-	conf = IO.readlines(CONFIGURATION).reverse.map(&:strip).reject { |x| x.strip.start_with?(?#).|(x.empty?) }
+	conf = IO.readlines(CONFIGURATION).reverse.map!(&:strip).reject! { |x| x.strip.start_with?(?#).|(x.empty?) }
 	conf_reader = lambda { |a, default = ''| conf.find { |x| x.split(?=)[0].to_s.strip.downcase.eql?(a.downcase) }
 		.to_s.split(?=)[1].to_s.strip.then { |x| x.empty? ? default : x }  }
 
 	time_format = conf_reader.('time format', '%H:%M:%S')
 	refresh = conf_reader.('refresh', '0.05').to_f
 	animate = conf_reader.('animation', 'false').downcase == 'true'
-	colours = conf_reader.('colours', '129').split(?,).map(&:strip).then { |x| x.size < 1 ? [10, 10] : x.size == 1 ? x + x : x }
+	colours = conf_reader.('colours', '129').split(?,).map!(&:strip).then { |x| x.size < 1 ? [10, 10] : x.size == 1 ? x + x : x }
 	pattern = conf_reader.('animation pattern', ?1).to_i.then { |x| x < 1 || x > 12 ? 1 : x }
 	unit = conf_reader.('unit', 'mb').upcase
 
 	bar_colour = conf_reader.('bar colour', '-1').to_i.then { |x| x < -1 ? 129 : x > 255 ? 129 : x }
 	bar_text_colour = conf_reader.('bar text colour', '255').to_i.then { |x| x < 0 ? 255 : x }
-	bar_text_anim_colour = 	conf_reader.('bar text animate colours', '10').split(?,).map(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
+	bar_text_anim_colour = 	conf_reader.('bar text animate colours', '10').split(?,).map!(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
 	bar_text_anim = conf_reader.('bar text animation pattern', ?1).to_i
 
-	username = conf_reader.('username', 'auto').then { |x| x == 'auto' ? ENV['USER'].to_s : x }.split.map(&:capitalize).join.strip.then { |x| "\xF0\x9F\x91\xA4 #{x}" }
+	username = conf_reader.('username', 'auto').then { |x| x == 'auto' ? ENV['USER'].to_s : x }.split.map!(&:capitalize).join.strip.then { |x| "\xF0\x9F\x91\xA4 #{x}" }
 
 	display_message = conf_reader.('display message', 'true') != 'false'
-	message_colours = conf_reader.('message colours', '129').split(?,).map(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
+	message_colours = conf_reader.('message colours', '129').split(?,).map!(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
 	message_animation_pattern = conf_reader.('message animation pattern', ?1).to_i
 
 	display_quote = conf_reader.('display quote', 'true') != 'false'
-	quote_colours = conf_reader.('quote colours', '184, 208, 203, 198, 164, 129, 92').split(?,).map(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
+	quote_colours = conf_reader.('quote colours', '184, 208, 203, 198, 164, 129, 92').split(?,).map!(&:strip).then { |x| x.size < 1 ? [129, 129] : x.size == 1 ? x + x : x }
 	quote_animation_pattern = conf_reader.('quote animation pattern', ?0).to_i
 	quote_refresh_time = conf_reader.('quote refresh time', '15').to_f
 
 	puts "\e[?25l" if conf_reader.('hide cursor', 'false') == 'true'
 
-	display = proc { |c| c.to_s.chars.map { |x| x.upcase.then { |y| y.eql?(?\s) ? y : characters[y] } }
-		.then { |y| y[0].to_s.split(?\n).size.times.map { |i| y.map { |z| z.split(?\n)[i] }.join.delete(?\n) }.join(?\n) } }
+	display = proc do |c|
+		c.to_s.chars.map! { |x| x.upcase.then { |y| characters.fetch(y) { y } } }
+			.then { |y| y[0].to_s.split($-n).size.times.map { |i| y.map { |z| z.lines[i] }.join.delete($-n) }.join($-n) }
+	end
 
 	if display_quote
 		if File.readable?(QUOTE)
-			quotes = IO.readlines(QUOTE).uniq.map { |x| x.split(?\t).values_at(1, 0).map(&:strip).join("    -") }
-			quote_refreshed = Time.new.strftime('%s').to_i
+			quotes = IO.readlines(QUOTE).uniq.map! { |x| x.split(?\t).values_at(1, 0).map!(&:strip).join("    -") }
+			quote_refreshed = Time.now.to_i
 		else
 			Kernel.warn(QUOTE.colourize + (File.exist?(QUOTE) ? ' is not readable' : ' does not exist').+('... Disabling quotes.').colourize)
 			Kernel.warn("You may download quotes with `#{File.basename($0)} --download-quote' option".colourize)
@@ -161,32 +168,29 @@ def main
 
 	q, message = display_quote ? quotes.sample : '', ''
 
-	clocks = "\xF0\x9F\x95\x8F".then { |x| 12.times.map { |y| x.next!.dup } }
-	counter, anim_bars = 0
+	clocks, counter = "\xF0\x9F\x95\x8F".then { |x| 12.times.map { |y| x.next!.dup } }, 0
 	anim_bars = %W(\xE2\xA0\x82 \xE2\xA0\x92 \xE2\xA0\xB2 \xE2\xA0\xB6 \xE2\xA0\xA2 \xE2\xA0\xA2 \xE2\xA0\xA2 \xE2\xA0\xA2\xE2\xA0\xA2
 					\xE2\xA0\x87 \xE2\xA0\x87 \xE2\xA0\x87 \xE2\xA0\x94 \xE2\xA0\x94 \xE2\xA0\x94 \xE2\xA0\x92 \xE2\xA0\x92)
 	anim_bars2 = %W(\xE2\xA0\x81 \xE2\xA0\x82 \xE2\xA0\x84 \xE2\xA0\x91 \xE2\xA0\x8A)
 	quote_counter, quote_anim, anim_quote, final_quote = -1, '', '', ''
-	output = ''
 
 	loop do
 		width = STDOUT.winsize[1]
 
 		# Calculate Memory Usage
-		mem_used = if File.readable?('/proc/meminfo')
-			mem_total, mem_available = IO.readlines('/proc/meminfo').values_at(0, 2).map { |x| x.split[1].to_f }
+		mem_used = if File.readable?(MEMINFO)
+			mem_total, mem_available = IO.readlines(MEMINFO).values_at(0, 2).map! { |x| x.split[1].to_f }
 			mem_total - mem_available
 		else
 			mem_total = mem_available = 0.0
-			0.0
 		end
 
 		# Calculate Swap Usage
-		swap_stats = if File.readable?('/proc/swaps')
-			swap_devs = IO.readlines('/proc/swaps').drop(1)
+		swap_stats = if File.readable?(SWAPINFO)
+			swap_devs = IO.readlines(SWAPINFO).drop(1)
 			swap_total, swap_used = swap_devs.map { |x| x.split[2].to_f }.sum, swap_devs.map { |x| x.split[3].to_f }.sum
 			unless swap_total.zero?
-				" | \xF0\x9F\x92\x9E Swap: #{swap_used.send(:/, unit == 'MIB' ? 1024.0 : 1000.0).rpad} #{unit}/#{swap_total.send(:/, unit == 'MIB' ? 1024.0 : 1000.0).rpad} #{unit}"
+				" | \xF0\x9F\x92\x9E Swap: #{swap_used.send(:/, unit == 'MIB'.freeze ? 1024.0 : 1000.0).rpad} #{unit}/#{swap_total.send(:/, unit == 'MIB'.freeze ? 1024.0 : 1000.0).rpad} #{unit}"
 			else
 				''
 			end
@@ -195,13 +199,13 @@ def main
 		end
 
 		# Calculate battery usage
-		battery = if File.readable?('/sys/class/power_supply/BAT0/charge_now')
+		battery = if File.readable?('/sys/class/power_supply/BAT0/charge_now'.freeze)
 			begin
-				status = IO.read('/sys/class/power_supply/BAT0/status').strip.downcase
-				if status == 'full' then " | \xE2\x9A\xA1"
-				elsif status == 'discharging' then " | \xF0\x9F\x94\x8B"
-				else " | \xF0\x9F\x94\x8C"
-				end + ' Battery: ' + IO.read('/sys/class/power_supply/BAT0/charge_now').to_i.*(100.0)./(IO.read('/sys/class/power_supply/BAT0/charge_full').to_i).rpad.rjust(4) + ?%
+				status = IO.read('/sys/class/power_supply/BAT0/status'.freeze).strip.downcase
+				if status == 'full'.freeze then " | \xE2\x9A\xA1".freeze
+				elsif status == 'discharging'.freeze then " | \xF0\x9F\x94\x8B".freeze
+				else " | \xF0\x9F\x94\x8C".freeze
+				end + ' Battery: ' + IO.read('/sys/class/power_supply/BAT0/charge_now'.freeze).to_i.*(100.0)./(IO.read('/sys/class/power_supply/BAT0/charge_full'.freeze).to_i).rpad.rjust(4) + ?%
 			rescue Exception
 				''
 			end
@@ -210,12 +214,12 @@ def main
 		end
 
 		# Calculate CPU usage
-		cpu_usage = if File.readable?('/proc/stat')
-			prev_file = IO.readlines('/proc/stat').select { |line| line.start_with?('cpu') }
+		cpu_usage = if File.readable?(CPU_USAGE)
+			prev_file = IO.readlines(CPU_USAGE).select! { |line| line.start_with?('cpu'.freeze) }
 			Kernel.sleep(refresh)
-			file = IO.readlines('/proc/stat').select { |line| line.start_with?('cpu') }
+			file = IO.readlines(CPU_USAGE).select! { |line| line.start_with?('cpu'.freeze) }
 
-			data, prev_data = file[0].split.map(&:to_f), prev_file[0].split.map(&:to_f)
+			data, prev_data = file[0].split.map!(&:to_f), prev_file[0].split.map!(&:to_f)
 
 			%w(user nice sys idle iowait irq softirq steal).each_with_index { |e, i| binding.eval "@#{e}, @prev_#{e} = #{data[i += 1]}, #{prev_data[i]}" }
 
@@ -231,49 +235,50 @@ def main
 
 		message.replace(
 			case Time.new.hour
-				when 5..11 then "\xF0\x9F\x8C\x85 Good Morning \xF0\x9F\x8C\x85"
-				when 12..16 then "\xF0\x9F\x8C\x87 Good Afternoon \xF0\x9F\x8C\x87"
-				when 17..19 then "\xF0\x9F\x8C\x86 Good Evening \xF0\x9F\x8C\x86"
-				else "\xF0\x9F\x8C\x83 Good Night \xF0\x9F\x8C\x83"
+				when 5..11 then "\xF0\x9F\x8C\x85 Good Morning \xF0\x9F\x8C\x85".freeze
+				when 12..16 then "\xF0\x9F\x8C\x87 Good Afternoon \xF0\x9F\x8C\x87".freeze
+				when 17..19 then "\xF0\x9F\x8C\x86 Good Evening \xF0\x9F\x8C\x86".freeze
+				else "\xF0\x9F\x8C\x83 Good Night \xF0\x9F\x8C\x83".freeze
 			end
 		) if display_message
 
 		if display_quote
+			posix_time = Time.now.to_i
+
 			if anim_quote.empty?
-				quote_refreshed, quote_counter = Time.new.strftime('%s').to_i, -1
+				quote_refreshed, quote_counter = posix_time, -1
 				q.replace(quotes.sample)
 				anim_quote.clear
 			end
 
-			unless anim_quote.length.eql?(q.length) || Time.new.strftime('%s').to_i > quote_refreshed + quote_refresh_time
+			unless anim_quote.length.eql?(q.length) || posix_time > quote_refreshed + quote_refresh_time
 				anim_quote << q[quote_counter += 1].to_s
 				quote_anim.replace(anim_bars.rotate![0])
 			else
 				quote_anim.replace(anim_bars2.rotate![0])
 			end
 
-			if Time.new.strftime('%s').to_i > quote_refreshed + quote_refresh_time
-				final_quote.replace(((quote_anim[0] + ?\s + anim_quote.chop!.to_s + ?|).center(width)).colourize + "\e[0m\n\n")
+			if posix_time > quote_refreshed + quote_refresh_time
+				final_quote.replace(((quote_anim[0] + $-s + anim_quote.chop!.to_s + ?|).center(width)).colourize + "\e[0m\n\n")
 			else
-				final_quote.replace(((quote_anim[0] + ?\s + anim_quote).center(width).rstrip).colourize + "\e[5m|\e[0m\n\n")
+				final_quote.replace(((quote_anim[0] + $-s + anim_quote).center(width).rstrip).colourize + "\e[5m|\e[0m\n\n")
 			end
 		end
 
 		info = "#{username} | #{clocks[(counter += 1) % clocks.size]} #{Time.new.strftime('%a, %b %D')}"\
-			" | \xF0\x9F\x92\xAD Memory: #{mem_used.send(:/, unit == 'MIB' ? 1024.0 : 1000.0).rpad} #{unit}/#{mem_total.send(:/, unit == 'MIB' ? 1024.0 : 1000.0).rpad} #{unit}"\
+			" | \xF0\x9F\x92\xAD Memory: #{mem_used.send(:/, unit == 'MIB'.freeze ? 1024.0 : 1000.0).rpad} #{unit}/#{mem_total.send(:/, unit == 'MIB'.freeze ? 1024.0 : 1000.0).rpad} #{unit}"\
 				"#{swap_stats}#{cpu_usage}#{battery}".center(width - 6)
 
-		# Print to the STDOUT. replace output everytime and then print everything to avoid waiting for calculations after the screen is cleared.
-		output.replace(
-			(bar_colour != -1 ? info.chars.map { |x| "\e[48;5;#{bar_colour}m\e[38;5;#{bar_text_colour}m#{x}\e[0m" }.join: info.colourize(colours: bar_text_anim_colour)) +
-			?\s * (STDOUT.winsize[0]./(3) * width) +
-			display.(Time.new.strftime(time_format)).each_line.map { |x| x.chomp.+(?\n).then { |y| ?\s * width./(2).-(y.length / 2).abs + y } }
-				.join.colourize(colours: colours, animate: animate, pattern: pattern).strip + ?\n +
-			final_quote +
-			message.center(width - 2).colourize(colours: message_colours)
-		)
-		# puts "\e[2J\e[H\e[3J#{output}\e[0m"
-		print "#{CLEAR}#{output}"
+		# Print to the STDOUT
+		print <<-EOF
+			#{CLEAR}#{
+			(bar_colour != -1 ? info.chars.map! { |x| "\e[48;5;#{bar_colour}m\e[38;5;#{bar_text_colour}m#{x}\e[0m" }.join: info.colourize(colours: bar_text_anim_colour))
+			} #{$-s * (STDOUT.winsize[0]./(3) * width)}#{
+			display.(Time.new.strftime(time_format)).lines.map! { |x| x.chomp.+($-n).then { |y| $-s * width./(2).-(y.length / 2).abs + y } }
+				.join.colourize(colours: colours, animate: animate, pattern: pattern).strip + $-n +
+			final_quote + message.center(width - 2).colourize(colours: message_colours)}
+		EOF
+
 		case pattern
 			when 3, 5 then colours.rotate!
 			when 4, 6 then colours.rotate!(-1)
@@ -318,7 +323,7 @@ begin
 		generate_files(CHARACTERS, 'https://raw.githubusercontent.com/Souravgoswami/term-clock/master/term-clock/characters.txt')
 
 	elsif ARGV.any? { |x| x[/^\-\-help/] || x[/^\-h$/] }
-		STDOUT.puts <<~EOF.each_line.map(&:colourize).join
+		STDOUT.puts <<~EOF.lines.map!(&:colourize).join
 			This is term-clock. A lightweight digital clock for your GNU/Linux system.
 
 			Configuration: The configuration can be found in #{CONFIGURATION}.
@@ -353,13 +358,13 @@ begin
 
 	elsif ARGV[0].to_s[/^\-\-colours$/]
 		STDOUT.print 15.then { |i| 6.times.map { 6.times.map { 6.times.map { "\e[48;5;#{i += 1}m#{i.to_s.center(8)}\e[0m" }.join } }
-        		.each_slice(STDOUT.winsize[1] / 49).map { |x| 6.times.map { |y| x.map { |z| z.at(y) }.join(?\s) }.join(?\n) + ?\n }.join(?\n) }
+        		.each_slice(STDOUT.winsize[1] / 49).map { |x| 6.times.map { |y| x.map { |z| z.at(y) }.join($-s) }.join($-n) + $-n }.join($-n) }
 
 	elsif ARGV[0].to_s[/^\-\-tty\?$/]
 		unless STDOUT.tty?
-			abort 'No Terminal Running! This program should support Tilix (terminix), GNOME Terminal, XFCE Terminal, LX '.colourize + ?\n +
-				'Terminal, Konsole, Terminilogy, XTerm, UXterm, etc.'.colourize + ?\n +
-				'Terminals like Cool-Retro-Term may not display emoji correctly.'.colourize + ?\n +
+			abort 'No Terminal Running! This program should support Tilix (terminix), GNOME Terminal, XFCE Terminal, LX '.colourize + $-n +
+				'Terminal, Konsole, Terminilogy, XTerm, UXterm, etc.'.colourize + $-n +
+				'Terminals like Cool-Retro-Term may not display emoji correctly.'.colourize + $-n +
 				'Also note to run this program to all intents and purposes, you need noto-fonts and noto-fonts-emoji to display characters and emojis.'.colourize
 		else
 			<<~EOF.colourize.each_char { |x| STDOUT.print(x) || Kernel.sleep(0.001) }
@@ -377,11 +382,11 @@ begin
 		main
 
 	elsif !ARGV.empty?
-		STDERR.puts "Invalid Argument #{ARGV[0].dump}!".colourize + ?\n * 2 +
+		STDERR.puts "Invalid Argument #{ARGV[0].dump}!".colourize + $-n * 2 +
 		"Avaiable Arguments are:
 		(1) --download-conf (2) --download-quote (3) --download-characters
 		(4) --download-all  (5) --help / -h      (6) --version / -v
-		(7) --colours       (8) --tty?           (9) --no-colour\n".colourize + ?\n +
+		(7) --colours       (8) --tty?           (9) --no-colour\n".colourize + $-n +
 		"Please run #{$0} --help/-h for more information...".colourize
 	else
 		main
@@ -393,11 +398,11 @@ rescue SignalException, Interrupt, SystemExit
 rescue Errno::ENOTTY
 	puts "Uh Oh! No terminal found. Please Run this in a terminal.\nOptionally run #{$0} --tty? to check for a TTY".colourize
 
-rescue Exception => e
+rescue Exception
 	puts "Uh oh! An Error Occurred...\n".colourize +
-		"#{e.to_s}: ".colourize + ?\n +
-		e.backtrace.map { |x| "  #{x}" }.join(?\n).colourize + ?\n +
-		?-.*(50).colourize + ?\n +
+		"#{$!.to_s}: ".colourize + $-n +
+		$!.backtrace.map { |x| "  #{x}" }.join($-n).colourize + $-n +
+		?-.*(50).colourize + $-n +
 		"Bug reports are appreciated".colourize
 ensure
 	puts "\e[?25h\e[0m\n"
